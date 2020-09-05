22b136e5fc6e85440651948000cc09b928da0d7f
nicase <nicase99@gmail.com>
2020-09-05 23:19:10 +0200

    ready for apocalypsis

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ s.start();
 console.log("susceptible: " + s.infected()[0].toString());
 console.log("infected: " + s.infected()[1].toString());
 console.log("recovered: " + s.infected()[2].toString());
+
+console.log(document.getElementById("textInput1").value)
+console.log(document.getElementById("textInput2").value)
+console.log(document.getElementById("textInput3").value)
+console.log(document.getElementById("textInput4").value)
