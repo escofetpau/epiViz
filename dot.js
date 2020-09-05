427e5f3d185e9cb502a77556f251bbe11f98a788
marc <marquitus99@gmail.com>
2020-09-05 13:47:09 +0200

    working on dot movement

--- dot.js
+++ dot.js
@@ -9,9 +9,12 @@ class Dot {
     //  0 I
     //  1 R
 
-    this.x = 0;
-    this.y = 0;
-    this.size = 70;
+    this.x = -300;
+    this.y = -100;
+    this.vel = {x: 0, y: 0};
+    this.acc = {x: 0, y: 0};
+    this.size = 30;
+    this.destination = {x: 300, y: 100};
   }
 
   newPleasure(nPleasures) {
@@ -38,4 +41,45 @@ class Dot {
     if (this.state === 0) return "red";
     if (this.state === 1) return "blue";
   }
+
+  setAcc(acc) {
+    let vector = {x: this.destination.x - this.x, y: this.destination.y - this.y};
+    let modul = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+    this.acc = {x: vector.x * acc / modul, y: vector.y * acc / modul};
+
+  }
+
+  move() {
+    this.vel.x += this.acc.x;
+    this.vel.y += this.acc.y;
+    this.x += this.vel.x;
+    this.y += this.vel.y;
+  }
+
+  goTo(destination, acc) {
+    this.fromPos = this.pos();
+    this.travelDistance = dist(this.fromPos, destination);
+    this.destination = destination;
+    this.setAcc(acc);
+  }
+
+  stop() {
+    this.vel = {x: 0, y: 0};
+    this.acc = {x: 0, y: 0};
+  }
+
+  onDestination() {
+    console.log(dist(this.pos(), this.destination));
+    return dist(this.pos(), this.destination) < 3;
+  }
+
+  pos() {
+    return {x: this.x, y: this.y}
+  }
+}
+
+function dist (A, B) {
+  let c = A.x - B.x; 
+  let d = A.y - B.y; 
+  return Math.sqrt(c*c + d*d);
 }
--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const d = new DrawTool("myCanvas", window.innerHeight, window.innerWidth);
 d.translate(d.width/2, d.height/2);
-d.background("green");
 
 var dot = new Dot(undefined, undefined, 1);
 
-dot.show(d);
+dot.setAcc(1);
+dot.x = -300;
+dot.y = -100;
+dot.destination = {x: 300, y: 100};
+
+function update() {
+    if (dot.onDestination()) dot.stop();
+    dot.move();
+    d.clearAll();
+    dot.show(d);
+    
+}
+
+d.setInterval(update, 10);
