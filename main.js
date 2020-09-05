d4298684dc0de07305abd9bf7dd652378f6e798c
Pau Escofet <pau.escofet@est.fib.upc.edu>
2020-09-05 23:17:17 +0200

    ready for disaster

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const s = new Simulation(500, 0.05, 10, 30);
+const s = new Simulation(500, 0.05, 10, 30, document);
 s.start();
 console.log("susceptible: " + s.infected()[0].toString());
 console.log("infected: " + s.infected()[1].toString());
