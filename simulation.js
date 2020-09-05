1dfbed2e0c35bfa781ef4ae11966c97eb71a7b5f
marc <marquitus99@gmail.com>
2020-09-05 23:14:25 +0200

    adding modals

--- simulation.js
+++ simulation.js
@@ -334,6 +334,7 @@ class Simulation {
         data: this.chartData
         });
         chart.render();
+        document.getElementsByClassName("canvasjs-chart-credit")[0].remove();
     }
 
     calculate_pos(pos, elem) {
