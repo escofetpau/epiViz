144efe64606c2934cd27f39097fe1b051bf2ccd2
marc <marquitus99@gmail.com>
2020-09-05 11:42:29 +0200

    coses

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const d = new DrawTool("myCanvas", window.innerHeight, window.innerWidth);
+d.translate(d.width/2, d.height/2);
 d.background("green");
 
 var dot = new Dot(undefined, undefined, 1);
