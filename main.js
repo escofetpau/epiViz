7be8bace695aea60d262cf49b118a3ccdc6a2be3
marc <marquitus99@gmail.com>
2020-09-06 02:23:02 +0200

    about proj text

--- main.js
+++ main.js
@@ -26,8 +26,26 @@ function openModal() {
   modal.style.visibility = "visible";
 }
 
-var aboutProjText = "Text1";
+
+var aboutProjText = `
+This project is a visualisation of the spreading of a pandemic. We have used a simple epidemiologic model called
+<a href="https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIR_model">SIR</a>, which defines 
+three states a person can be in: <i>Susceptible, Infectious and Recovered or Removed.</i> This is a simple model 
+that is capable of representing a lot of situations that we can find in a pandemic.
+<br><br>
+In our case, we decided to represent three scenarios. Each person (represented as a dot) has a family, a job, and
+goes to different recreational places, which change every day. You can think of these places as a Supermarket, a
+bar, a bus, a clothes shop, etc. A person wakes up at home, goes to work, and then goes to a random recreational
+location, and in each place interactuates (directly or inderectly, by touching the same things as others) with
+every person that is or was there. Each infected person has a probability (infection ratio) of infecting every 
+person he has interactuated with. This ratio is based on the sanity measures both interactors are doing, such as
+cleaning your hands frequently, wearing mask and keeping social distance.  
+<br><br>
+`;
 var aboutUsText = "Text2";
+
+document.getElementById("contentText").innerHTML = aboutProjText;
+
 
 function checkChoice(id) {
     let otherId;
@@ -50,5 +68,5 @@ function checkChoice(id) {
     other.style.backgroundColor = "white";
     other.style.color = "black";
 
-    document.getElementById("contentText").innerText = contentText;
+    document.getElementById("contentText").innerHTML = contentText;
 }
